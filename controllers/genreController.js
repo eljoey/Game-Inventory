0f25916f0ca218224b0c9cea5f83650689c16017
Joe Hayden <eljoeyy@gmail.com>
2019-11-11 13:24:59 -0600

    genre_detail route, view  and create genre handling

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -5,7 +5,7 @@ const validator = require('express-validator')
 
 const { body, validationResult, sanitizeBody } = require('express-validator')
 
-exports.genre_list = function(req, res) {
+exports.genre_list = function(req, res, next) {
   Genre.find()
     .sort('name')
     .exec(function(err, list_genres) {
@@ -19,15 +19,80 @@ exports.genre_list = function(req, res) {
       })
     })
 }
-exports.genre_detail = function(req, res) {
-  res.send('Single Genre - TODO')
+exports.genre_detail = function(req, res, next) {
+  async.parallel(
+    {
+      genre: callback => {
+        Genre.findById(req.params.id).exec(callback)
+      },
+      games: callback => {
+        Game.find({ genre: req.params.id }).exec(callback)
+      }
+    },
+    (err, result) => {
+      if (err) {
+        return next(err)
+      }
+
+      if (result.genre === null) {
+        const err = new Error('Genre not found')
+        err.status = 404
+        return next(err)
+      }
+
+      res.render('genre_detail', {
+        title: 'Genre Detail',
+        genre: result.genre,
+        games: result.games
+      })
+    }
+  )
 }
 exports.genre_create_get = function(req, res) {
-  res.send('Create Genre GET - TODO')
+  res.render('genre_form', {
+    title: 'Create Genre'
+  })
 }
-exports.genre_create_post = function(req, res) {
-  res.send('Create Genre POST - TODO')
-}
+exports.genre_create_post = [
+  validator
+    .body('name', 'Name must not be empty')
+    .trim()
+    .isLength({ min: 1 }),
+  validator.sanitizeBody('name').escape(),
+
+  (req, res, next) => {
+    const errors = validator.validationResult(req)
+
+    let genre = new Genre({ name: req.body.name })
+
+    if (!errors.isEmpty()) {
+      res.render('genre_form', {
+        title: 'Create Genre',
+        genre: genre,
+        errors: errors.array()
+      })
+      return
+    } else {
+      Genre.findOne({ name: req.body.name }).exec((err, found_genre) => {
+        if (err) {
+          return next(err)
+        }
+
+        if (found_genre) {
+          res.redirect(found_genre.url)
+        } else {
+          genre.save(err => {
+            if (err) {
+              return next(err)
+            }
+
+            res.redirect(genre.url)
+          })
+        }
+      })
+    }
+  }
+]
 exports.genre_delete_get = function(req, res) {
   res.send('Delete Genre GET - TODO')
 }
